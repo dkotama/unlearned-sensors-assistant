03ff522dfd5bf6ef974789486164428b113cdf13
Darma Kotama, I Nyoman <darma.kotama@gmail.com>
2025-04-24 15:39:51 +0900

    Update firebaseConfig.js

--- frontend/lib/firebaseConfig.js
+++ frontend/lib/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjpGWi4FvaoLg0GKuaRTYVqACd7CEesGg",
-  authDomain: "unlearned-sensor.firebaseapp.com",
-  projectId: "unlearned-sensor",
-  storageBucket: "unlearned-sensor.firebasestorage.app",
-  messagingSenderId: "468425508344",
-  appId: "1:468425508344:web:ab7924ffc6df9698bac20d",
-  measurementId: "G-V0GDXCQ9GN"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
